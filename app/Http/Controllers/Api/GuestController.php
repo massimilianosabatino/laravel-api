4e41a2dcab523b9d2a3c45dac0dc91d742f0aa10
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-25 17:12:57 +0200

    add: first api interaction

---
+++ app/Http/Controllers/Api/GuestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GuestController extends Controller
+{
+    public function index() {
+        return 'funziona da controller';
+    }
+}
