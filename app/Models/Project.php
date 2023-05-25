4698c7c85ab1e365234eb45663d2efd0f6761776
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-25 22:21:48 +0200

    add: accessor for cover image on API

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
@@ -19,5 +21,20 @@ class Project extends Model
     public function technologies()
     {
         return $this->belongsToMany(Technology::class)->withTimestamps();
+    }
+
+    protected function cover(): Attribute
+    {
+        return Attribute::make(
+            get: function (string|null $value) {
+                if ($value !== null AND Str::startsWith($value, 'upload')){
+                    return asset('storage/' . $value);
+                }
+                if ($value !== null AND Str::startsWith($value, 'http')) {
+                    return $value;
+                }
+                return null;
+            }
+        );
     }
 }
