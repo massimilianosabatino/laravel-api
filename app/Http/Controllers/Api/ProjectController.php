318a46c76c11f0843063e09ddcee767c252c7301
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-25 17:20:23 +0200

    add: get response from api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        return 'funziona da controller';
+        
+        $projects = Project::all();
+
+        return response()->json([
+            'succes' => true,
+            'results' => $projects
+        ]);
     }
 }
