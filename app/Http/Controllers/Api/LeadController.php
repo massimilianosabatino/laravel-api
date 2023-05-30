f2b26d38bbf589d6c8fee74169af1089e5570087
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-31 01:43:08 +0200

    feat: api new lead

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewMessage;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+
+        Mail::to('info@boolfolio.com')->send(new NewMessage($newLead));
+
+        return $newLead;
+    }
+}
